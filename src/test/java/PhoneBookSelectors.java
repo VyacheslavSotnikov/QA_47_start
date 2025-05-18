769c49da375b9bc2b19267662b6cf534144de811
VyacheslavSotnikov <sotiga2007@gmail.com>
2025-05-18 22:23:45 +0300

    created HW tests

--- src/test/java/PhoneBookSelectors.java
+++ src/test/java/PhoneBookSelectors.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -16,11 +18,25 @@ public class PhoneBookSelectors {
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+        FirstTest.pause(3);
+        WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
+        btnAbout.click();
 
-        // HW
-        //
-        FirstTest.pause(5);
-        }
+        FirstTest.pause(3);
+        WebElement TextOfBodyAbout = driver.findElement(By.className("about_main__2Uv5W"));
+        System.out.println(TextOfBodyAbout.getText());
+
+        FirstTest.pause(3);
+        WebElement btnLogin = driver.findElement(By.cssSelector("a[href='/login']"));
+        btnLogin.click();
+
+        FirstTest.pause(3);
+        WebElement InputEmail = driver.findElement(By.cssSelector("input[placeholder='Email']"));
+        InputEmail.sendKeys("sotiga2007@gmail.com");
+
+        FirstTest.pause(3);
+        WebElement InputPassword = driver.findElement(By.cssSelector("input[placeholder='Password']"));
+        InputPassword.sendKeys("12345");
     }
-
+}
 
